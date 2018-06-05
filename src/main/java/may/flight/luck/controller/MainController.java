efe01c2d5451abc25393e916745b5b7c98956ad4
mayflight <18868824465@163.com>
2018-06-05 11:35:18 +0800

    创建工程

---
+++ src/main/java/may/flight/luck/controller/MainController.java
@@ -0,0 +1,25 @@
+package may.flight.luck.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("main/")
+public class MainController {
+    @RequestMapping("start.htm")
+    public void start(HttpServletRequest request, HttpServletResponse response, String text) {
+        response.setCharacterEncoding("utf-8");
+        if (StringUtils.isEmpty(text)) {
+            text = "spring mvc ¿ªÊ¼";
+        }
+        try {
+            response.getWriter().print(text);
+        } catch (Exception e) {
+
+        }
+    }
+
+}
