048cd4ad05e7a855c3e8ec5e05776698221f8ffa
mayflight <18868824465@163.com>
2018-06-25 16:15:30 +0800

    log日志

--- src/main/java/may/flight/luck/controller/MainController.java
+++ src/main/java/may/flight/luck/controller/MainController.java
@@ -1,19 +1,28 @@
 package may.flight.luck.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 @Controller
 @RequestMapping("main/")
 public class MainController extends BaseController {
+
+   private static Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @RequestMapping("start.htm")
     public void start(HttpServletRequest request, HttpServletResponse response, String text) {
+        logger.error("开始{}", request.getRemoteHost());
         text = StringUtils.isEmpty(text) ? "据说看到这个页面的人会有好运哦!" : text;
         print(response, text);
     }
+
+
 
 }
