d7e839acc1171a34907f3ca2a5bf9a511995b5dc
mayflight <18868824465@163.com>
2018-07-05 14:23:34 +0800

    AOP

--- src/main/java/may/flight/luck/controller/MainController.java
+++ src/main/java/may/flight/luck/controller/MainController.java
@@ -1,13 +1,17 @@
 package may.flight.luck.controller;
 
 
+import may.flight.luck.service.AllMessageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 
 @Controller
@@ -15,6 +19,8 @@ import javax.servlet.http.HttpServletResponse;
 public class MainController extends BaseController {
 
    private static Logger logger = LoggerFactory.getLogger(MainController.class);
+   @Resource
+   private AllMessageService allMessageService;
 
     @RequestMapping("start.htm")
     public void start(HttpServletRequest request, HttpServletResponse response, String text) {
@@ -23,6 +29,11 @@ public class MainController extends BaseController {
         print(response, text);
     }
 
+   @RequestMapping("rule.htm")
+    public void rule(HttpServletRequest request, HttpServletResponse response) {
+       Map<String,String> map = allMessageService.getNameAgeMap();
+       print(response, map.toString());
+   }
 
 
 }
