004873c60cff8a8f1ff86b4638ea36878dd8f862
mayflight <18868824465@163.com>
2018-07-06 15:15:08 +0800

    memcached

--- src/main/java/may/flight/luck/controller/MainController.java
+++ src/main/java/may/flight/luck/controller/MainController.java
@@ -1,9 +1,11 @@
 package may.flight.luck.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import may.flight.luck.service.AllMessageService;
 import may.flight.luck.utils.IpUtil;
 import may.flight.luck.utils.QueryStringUtils;
 import may.flight.luck.utils.StreamUtil;
+import net.spy.memcached.MemcachedClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -23,13 +25,22 @@ public class MainController extends BaseController {
    private static Logger logger = LoggerFactory.getLogger(MainController.class);
    @Resource
    private AllMessageService allMessageService;
+   @Resource
+   private MemcachedClient memcachedClient;
 
     @RequestMapping("start.htm")
     public void start(HttpServletRequest request, HttpServletResponse response, String text) {
-        String[] logParams =  {IpUtil.clientIp(request), QueryStringUtils.getQueryString(request), StreamUtil.readRequestInputStream(request)};
+        Object[] logParams =  {IpUtil.clientIp(request), QueryStringUtils.getQueryString(request), StreamUtil.readRequestInputStream(request)};
         logger.error("client_ip:{}, request_params:{}, request_body:{}", logParams);
+        memcachedClient.set("request_params", 60, logParams);
         text = StringUtils.isEmpty(text) ? "据说看到这个页面的人会有好运哦!" : text;
         print(response, text);
+    }
+
+    @RequestMapping("printLog.htm")
+    public void printLog(HttpServletRequest request, HttpServletResponse response) {
+        Object params =  memcachedClient.get("request_params");
+        print(response, JSONObject.toJSONString(params));
     }
 
    @RequestMapping("rule.htm")
