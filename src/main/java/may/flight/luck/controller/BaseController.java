bf35482eb0cbdcdb2152d754ceaca3e002607066
mayflight <18868824465@163.com>
2018-07-11 14:07:38 +0800

    text

--- src/main/java/may/flight/luck/controller/BaseController.java
+++ src/main/java/may/flight/luck/controller/BaseController.java
@@ -1,8 +1,21 @@
 package may.flight.luck.controller;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class BaseController {
+
+    protected HttpServletRequest request;
+
+    public HttpServletRequest getRequest() {
+        return request;
+    }
+    @Resource
+    public void setRequest(HttpServletRequest request) {
+        this.request = request;
+    }
+
     public void print(HttpServletResponse response, String text) {
         if (null == response) {
             return;
