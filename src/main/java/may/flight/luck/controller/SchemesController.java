bfe4480f8252c53c8ad798fa1186529d463c62db
mayflight <18868824465@163.com>
2018-07-20 13:04:31 +0800

    jiuyan

--- src/main/java/may/flight/luck/controller/SchemesController.java
+++ src/main/java/may/flight/luck/controller/SchemesController.java
@@ -2,6 +2,7 @@ package may.flight.luck.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,9 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequestMapping("scheme/")
 public class SchemesController extends BaseController {
-    @RequestMapping("alipay/red_code.htm")
-    public String redCode(HttpServletRequest request, HttpServletResponse response, Model model) {
+    @RequestMapping("{id}/red_code.htm")
+    public String redCode(HttpServletRequest request, HttpServletResponse response, @PathVariable String id, Model model) {
         String redCode = "alipayqr://platformapi/startapp?saId=10000007&qrcode=https://qr.alipay.com/c1x03418odvudezogjca9ac";
+        if ("jiuyan".equals(id)) {
+            redCode = "alipayqr://platformapi/startapp?saId=10000007&qrcode=https://qr.alipay.com/c1x036533xmxf9ccpgbn3bc";
+        }
+
         model.addAttribute("code", redCode);
         return "red_code";
     }
