4b434d287670e8285224f083d3f82c0a9e75b763
mayflight <18868824465@163.com>
2019-02-20 10:34:49 +0800

    spi测试

---
+++ src/main/java/may/flight/luck/component/SpiTest.java
@@ -0,0 +1,12 @@
+package may.flight.luck.component;
+
+import com.yadong.ye.spi.Say;
+
+import java.util.ServiceLoader;
+
+public class SpiTest {
+    public static void main(String[] args) {
+        ServiceLoader<Say> loader = ServiceLoader.load(Say.class);
+        loader.forEach(Say::say);
+    }
+}
